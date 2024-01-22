6f0cc9207e966e1f65208c00c5513812712d2e0f
Stewart Lynch <slynch@createchsol.com>
2024-01-22 10:49:15 -0800

    Initial Commit

---
+++ Custom Calendar/Custom_CalendarApp.swift
@@ -0,0 +1,22 @@
+//
+// Created for Custom Calendar
+// by  Stewart Lynch on 2024-01-22
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+@main
+struct Custom_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
